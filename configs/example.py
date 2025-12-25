4ee9bd7ce0ebbb9bd7285f36c7a147827b963268
Piero Viscone <pieroviscone@gmail.com>
2025-12-25 03:57:12 +0100

    minimal configs for debugging debug

--- configs/example.py
+++ configs/example.py
@@ -2,16 +2,18 @@ from CutFinder.configs import ConfigObj, ConfigRef, GlobalConf
 
 import numpy as np
 
-glob = GlobalConf(pt_bins=np.linspace(0, 100, 101))
+glob = GlobalConf(pt_bins=np.arange(0, 65, 5))
 
 new_id = ConfigObj(
-    samples_path="root://eoscms.cern.ch//eos/cms/store/cmst3/group/l1tr/pviscone/l1teg/fp_ntuples/NuGunAllEta_PU200/FP/151X_TkElePtRegrTemp_A0/*.root",
+    samples_path="root://eoscms.cern.ch//eos/cms/store/cmst3/group/l1tr/pviscone/l1teg/fp_ntuples/NuGunAllEta_PU200/FP/151X_TkElePtRegrTemp_A0/perfNano_10044360_{95..100}.root",
     pt_branch="TkEleL2_pt",
     score_branch="TkEleL2_idScore",
 )
+custom = new_id.clone(ConfigRef, WP=[[0], [0]])
 
+"""
 elliptic = ConfigRef(
-    samples_path="root://eoscms.cern.ch//eos/cms/store/cmst3/group/l1tr/pviscone/l1teg/fp_ntuples/NuGunAllEta_PU200/FP/140Xv0C1/*.root",
+    samples_path="root://eoscms.cern.ch//eos/cms/store/cmst3/group/l1tr/pviscone/l1teg/fp_ntuples/NuGunAllEta_PU200/FP/140Xv0C1/perfNano_8843496_{95..100}.root",
     pt_branch="TkEleL2_pt",
 )
 
@@ -20,8 +22,8 @@ def tightID(rdf):
     return rdf.Redefine("TkEleL2_pt", "TkEleL2_pt[(TkEleL2_hwQual & 2)==2]").Redefine(
         "TkEleL2_idScore", "TkEleL2_idScore[(TkEleL2_hwQual & 2)==2]"
     )
+
+
 elliptic_tight = elliptic.clone(preprocess_function=tightID)
-custom = new_id.clone(ConfigRef, WP=[[0], [0]])
-
-
-#!Create refs argument in configObj ????
+"""
+#!Add in the example refs argument in configObj
