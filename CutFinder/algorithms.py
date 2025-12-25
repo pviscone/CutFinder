2eefa504192b25f63df712eda08ff8f58307a2cd
Piero Viscone <pieroviscone@gmail.com>
2025-12-25 03:56:08 +0100

    first try of the iterative_bin_cutter algorithm (to fix)

---
+++ CutFinder/algorithms.py
@@ -0,0 +1,130 @@
+import numpy as np
+
+# ! NB if scaling is applied, the cuts are computed on the offline pT scale.
+#! Inverse scaling will be applied in the regressor to map back to online pT.
+
+
+def iterative_bin_cutter(ref, obj, glob):
+    ref.makeRate(glob.pt_bins, glob.maxRate)
+
+    cuts = []
+
+    if ref.rate[-1] == 0.0:
+        print(
+            f"Warning: target rate in bin -1 ({glob.pt_bins[-1]} GeV) is zero (probably due to low stat). No cut will be applied."
+        )
+        cuts.append(-np.inf)
+    else:
+        # Select max score in the last pt bin
+        scores = (
+            obj.rdf.Define(
+                "scores", f"{obj.score_branch}[{obj.pt_branch} > {glob.pt_bins[-1]}]"
+            )
+            .Filter("scores.size() > 0")
+            .Define("max_score", "Max(scores)")
+        ).AsNumpy(["max_score"])["max_score"]
+        rate_bin = len(scores) * (glob.maxRate / obj.TotEvents)
+
+        # Fraction of events to keep in the last pt bin
+        f = ref.rate[-1] / rate_bin
+
+        # check if f>=1 otherwise no cut can be applied
+        if f > 1.0:
+            print(
+                f"Warning: target rate in bin -1 ({glob.pt_bins[-1]} GeV) is higher than current rate. No cut will be applied."
+            )
+            cuts.append(-np.inf)
+        elif len(scores) == 0:
+            print(
+                f"Warning: no events in the last pt bin {glob.pt_bins[-1]} GeV. No cut will be applied."
+            )
+            cuts.append(-np.inf)
+        else:
+            # find cut to achieve target rate in the last pt bin
+            cuts.append(np.quantile(scores, (1 - f)))
+
+    # Apply cut in the last bin
+    if cuts[-1] != -np.inf:
+        cut_mask = f"({obj.pt_branch} >= {glob.pt_bins[-1]} && {obj.score_branch} >= {cuts[-1]}) || ({obj.pt_branch} < {glob.pt_bins[-1]})"
+        rdf = (
+            obj.rdf.Define("cut_mask", cut_mask)
+            .Redefine(
+                obj.score_branch,
+                f"{obj.score_branch}[cut_mask]",
+            )
+            .Redefine(
+                obj.pt_branch,
+                f"{obj.pt_branch}[cut_mask]",
+            )
+            .Filter(f"{obj.pt_branch}.size() > 0")
+        )
+    else:
+        rdf = obj.rdf.Define(
+            "cut_mask", f"ROOT::VecOps::RVec<bool>({obj.pt_branch}.size(), true)"
+        )  # keep all events
+
+    for i in range(len(glob.pt_bins) - 2, -1, -1):
+        print(
+            f"Processing pt bin {i}: {glob.pt_bins[i]} - {glob.pt_bins[i + 1]} GeV (Obj: {obj.name}, Ref: {ref.name})"
+        )
+        # Select max score in the current pt bin (from the last to the first) and discard events which already triggered the previous cuts
+        scores = (
+            # select objs in current pt bin
+            rdf.Redefine(
+                obj.score_branch,
+                f"{obj.score_branch}[{obj.pt_branch} >= {glob.pt_bins[i - 1]} && {obj.pt_branch} < {glob.pt_bins[i]}]",
+            )
+            .Redefine(
+                obj.pt_branch,
+                f"{obj.pt_branch}[{obj.pt_branch} >= {glob.pt_bins[i - 1]} && {obj.pt_branch} < {glob.pt_bins[i]}]",
+            )
+            .Filter(f"{obj.pt_branch}.size() > 0")
+            # Discard events which already triggered previous cuts
+            .Filter(f"Max({obj.pt_branch}) < {glob.pt_bins[i + 1]}")
+            # Select max score
+            .Define("max_score", f"Max({obj.score_branch})")
+            .AsNumpy(["max_score"])["max_score"]
+        )
+
+        rate_bin = len(scores) * (glob.maxRate / obj.TotEvents) + ref.rate[i + 1]
+
+        f = (ref.rate[i] - ref.rate[i + 1]) / (rate_bin - ref.rate[i + 1])
+        if f > 1.0:
+            print(
+                f"Warning: target rate in bin {i} ({glob.pt_bins[i]} GeV) is higher than current rate. No cut will be applied."
+            )
+            cuts.append(-np.inf)
+        elif f < 0.0:
+            print(
+                f"Warning: target rate in bin {i} ({glob.pt_bins[i]} GeV) is lower than previous rate. No cut will be applied."
+            )
+            cuts.append(-np.inf)
+        else:
+            # find cut to achieve target rate in the current pt bin
+            if len(scores) == 0:
+                print(
+                    f"Warning: no events in the current pt bin {i} ({glob.pt_bins[i]} GeV). No cut will be applied."
+                )
+                cuts.append(-np.inf)
+            else:
+                cuts.append(np.quantile(scores, (1 - f)))
+
+        if (
+            cuts[-1] != -np.inf and i > 0
+        ):  # it's useless to apply cut in the first bin (last iteration)
+            cut_mask = f"({obj.pt_branch} >= {glob.pt_bins[i]} && {obj.pt_branch} < {glob.pt_bins[i + 1]} && {obj.score_branch} >= {cuts[-1]}) || ({obj.pt_branch} < {glob.pt_bins[i]})"
+            rdf = (
+                rdf.Redefine("cut_mask", cut_mask)
+                .Redefine(
+                    obj.score_branch,
+                    f"{obj.score_branch}[cut_mask]",
+                )
+                .Redefine(
+                    obj.pt_branch,
+                    f"{obj.pt_branch}[cut_mask]",
+                )
+                .Filter(f"{obj.pt_branch}.size() > 0")
+            )
+
+    cuts = np.array(cuts[::-1])
+    return cuts
