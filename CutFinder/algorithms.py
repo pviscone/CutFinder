f93bd54039253ae0dabd5f9c9d857227064ca629
Piero Viscone <pieroviscone@gmail.com>
2026-01-05 23:23:59 +0100

    added cuts_err with bootstrap

--- CutFinder/algorithms.py
+++ CutFinder/algorithms.py
@@ -4,15 +4,17 @@ import numpy as np
 #! Inverse scaling will be applied in the regressor to map back to online pT.
 
 
-def iterative_bin_cutter(ref, obj, glob, **kwargs):
+def iterative_bin_cutter(ref, obj, glob, n_bootstrap=1000):
     ref.makeRate(glob.pt_bins, glob.maxRate)
     cuts = []
+    cuts_err = []
 
     if ref.rate[-1] == 0.0:
         print(
             f"Warning: target rate in bin -1 ({glob.pt_bins[-1]} GeV) is zero (probably due to low stat). No cut will be applied."
         )
         cuts.append(-np.inf)
+        cuts_err.append(0.0)
     else:
         # Select max score in the last pt bin
         scores = (
@@ -49,9 +51,20 @@ def iterative_bin_cutter(ref, obj, glob, **kwargs):
                 f"Warning: no events in the last pt bin {glob.pt_bins[-1]} GeV. No cut will be applied."
             )
             cuts.append(-np.inf)
+            cuts_err.append(0.0)
         else:
             # find cut to achieve target rate in the last pt bin
-            cuts.append(np.quantile(scores, (1 - f)))
+            q = np.quantile(scores, 1 - f)
+            if len(scores) < 2 or n_bootstrap <= 0:
+                sd = 0.0
+            else:
+                idx = np.random.randint(0, len(scores), size=(n_bootstrap, len(scores)))
+                boot_q = np.quantile(scores[idx], 1 - f, axis=1)
+                sd = float(np.std(boot_q, ddof=1))
+            cuts.append(q)
+            cuts_err.append(sd)
+
+
 
     # Apply cut in the last bin
     if cuts[-1] != -np.inf:
@@ -103,11 +116,13 @@ def iterative_bin_cutter(ref, obj, glob, **kwargs):
                 f"Warning: target rate in bin {i} ({glob.pt_bins[i]} GeV) is higher than current rate. No cut will be applied."
             )
             cuts.append(-np.inf)
+            cuts_err.append(0.0)
         elif f < 0.0:
             print(
                 f"Warning: target rate in bin {i} ({glob.pt_bins[i]} GeV) is lower than previous rate. No cut will be applied."
             )
             cuts.append(-np.inf)
+            cuts_err.append(0.0)
         else:
             # find cut to achieve target rate in the current pt bin
             if len(scores) == 0:
@@ -115,8 +130,21 @@ def iterative_bin_cutter(ref, obj, glob, **kwargs):
                     f"Warning: no events in the current pt bin {i} ({glob.pt_bins[i]} GeV). No cut will be applied."
                 )
                 cuts.append(-np.inf)
+                cuts_err.append(0.0)
             else:
-                cuts.append(np.quantile(scores, (1 - f)))
+                q = np.quantile(scores, 1 - f)
+                if len(scores) < 2 or n_bootstrap <= 0:
+                    sd = 0.0
+                else:
+                    idx = np.random.randint(
+                        0, len(scores), size=(n_bootstrap, len(scores))
+                    )
+                    boot_q = np.quantile(scores[idx], 1 - f, axis=1)
+                    sd = float(np.std(boot_q, ddof=1))
+                cuts.append(q)
+                cuts_err.append(sd)
+
+
 
         if (
             cuts[-1] != -np.inf and i > 0
@@ -134,8 +162,9 @@ def iterative_bin_cutter(ref, obj, glob, **kwargs):
                 )
                 .Filter(f"{obj.pt_branch}.size() > 0")
             )
-        print(f"\tCut found : {cuts[-1]}\n")
+        print(f"\tCut found : {cuts[-1]} +- {cuts_err[-1]}\n", flush=True)
 
     cuts = np.array(cuts[::-1])
-    return cuts
+    cuts_err = np.array(cuts_err[::-1])
+    return cuts, cuts_err
 
